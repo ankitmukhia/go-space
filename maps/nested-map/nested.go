f4662e5a97d5f822dbdacce52ff880d45a8b4f4e
brookly 🥦 <ankitmukhia157@gmail.com>
2024-12-08 16:20:20 +0530

    adds nested map

--- maps/nested-map/nested.go
+++ maps/nested-map/nested.go
@@ -3,19 +3,22 @@ package nested
 func getNameCounts(names []string) map[rune]map[string]int {
 	res := make(map[rune]map[string]int)
 
-	for i, name := range names {
-			
+	for _, name := range names {
+		chars := []rune(name) // it will
+		for _, chr := range chars{
+			/* As for any given outer key(which is here 1st name char) you must check if the inner map exists, and create it if there is not */
+			rn, ok := res[chr]
+			if !ok {
+				rn = make(map[string]int)
+				/*
+				* '1st chr': map[string]int
+				* crete if not exists
+				**/
+				res[chr] = rn 
+			}
+			res[chr][name]++
+		}
 	}
 
 	return res
 }
-
-/* func add(m map[string]map[string]int, path, country string) {
-    mm, ok := m[path]
-    if !ok {
-        mm = make(map[string]int)
-        m[path] = mm
-    }
-    mm[country]++
-}
-add(hits, "/doc/", "au") */
